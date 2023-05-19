f23c46d2ed13b1708614bb0887bb9850bdf1ef1e
Lynnmich <lynnenmichuki@gmail.com>
2023-05-19 19:15:04 +0000

    Task

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""A script that starts a Flask web application & returns Hello HBNB & HBNB"""
+
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """Returns Hello HBNB"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Returns HBNB"""
+    return "HBNB"
+
+
+@app.route('/C/<text>', strict_slashes=False)
+def c(text):
+    """Returns C plus the value of the text variable"""
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
