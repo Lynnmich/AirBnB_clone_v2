b3e82258cc49e68d3f6937f4777a7ea432dbc024
Lynnmich <lynnenmichuki@gmail.com>
2023-05-19 16:31:18 +0000

    Task

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -6,10 +6,12 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """Returns a  string"""
     return "Hello HBNB!"
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """Returns HBNB"""
     return "HBNB"
 
 if __name__ == "__main__":
