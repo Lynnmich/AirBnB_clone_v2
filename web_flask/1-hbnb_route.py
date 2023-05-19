2ccc290944bd70a6d76d3e372cd34a7a98345104
Lynnmich <lynnenmichuki@gmail.com>
2023-05-19 17:40:49 +0000

    Task

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A script that starts a Flask web application and returns Hello HBNB & HBNB """
+"""A script that starts a Flask web application & returns Hello HBNB & HBNB"""
 
 
 from flask import Flask
