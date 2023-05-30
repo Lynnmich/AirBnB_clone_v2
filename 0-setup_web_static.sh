a9929bcc3a796cbb4d7b9ecedb662bcd993e484c
Lynne Michuki <lynnenmichuki@gmail.com>
2023-05-30 12:56:51 +0300

    Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -10,20 +10,14 @@ sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
 # Create a fake HTML file for testing
-echo "<html>
-  <head>
-  </head>
-  <body>
-    Holberton School
-  </body>
-</html>" | sudo tee /data/web_static/releases/test/index.html
+echo "Hello World!" | sudo tee /data/web_static/releases/test/index.html
 
 # Create or recreate symbolic link
 sudo rm -rf /data/web_static/current
-sudo ln -s /data/web_static/releases/test/ /data/web_static/current
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 # Give ownership of /data/ folder to ubuntu user and group
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -hR ubuntu:ubuntu /data/
 
 # Update Nginx configuration
 CONFIG_FILE="/etc/nginx/sites-available/default"
