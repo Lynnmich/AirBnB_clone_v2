2998b53d956dbd6dc5e6cec50190943783078580
Lynne Michuki <lynnenmichuki@gmail.com>
2023-05-30 11:30:10 +0300

    Update

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,27 +1,36 @@
 #!/usr/bin/env bash
 # Script that sets up a web server for deployment of web_static.
 
-#Update and Install Nginx
+# Update and Install Nginx
 sudo apt-get update
 sudo apt-get install -y nginx
 
-#Create folders if they don't exist
+# Create folders if they don't exist
 sudo mkdir -p /data/web_static/releases/test/
 sudo mkdir -p /data/web_static/shared/
 
-#Create a fake html file
-sudo echo "Hello World!" > /data/web_static/releases/test/index.html
+# Create a fake HTML file for testing
+echo "<html>
+  <head>
+  </head>
+  <body>
+    Holberton School
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-#Create a symbolic link
-sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create or recreate symbolic link
+sudo rm -rf /data/web_static/current
+sudo ln -s /data/web_static/releases/test/ /data/web_static/current
 
-#Give ownership of /data/ folder recursively
+# Give ownership of /data/ folder to ubuntu user and group
 sudo chown -R ubuntu:ubuntu /data/
 
-#Update Nginx config
-sudo sed -i '/^\s*location \/hbnb_static/ {
-    alias \/data\/web_static\/current\/;
-}' /etc/nginx/sites-available/default
+# Update Nginx configuration
+CONFIG_FILE="/etc/nginx/sites-available/default"
+CONFIG_BLOCK="location /hbnb_static {\n\talias /data/web_static/current/;\n}\n"
+sudo sed -i "/server_name _;/a $CONFIG_BLOCK" $CONFIG_FILE
 
-#Restart Nginx
+# Restart Nginx
 sudo service nginx restart
+
+exit 0
