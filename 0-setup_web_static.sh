38688f6d8efb8f6f8210f620b4def0ade1e89c80
Lynne Michuki <lynnenmichuki@gmail.com>
2023-05-30 13:00:58 +0300

    Task 0

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -20,9 +20,7 @@ sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 sudo chown -hR ubuntu:ubuntu /data/
 
 # Update Nginx configuration
-CONFIG_FILE="/etc/nginx/sites-available/default"
-CONFIG_BLOCK="location /hbnb_static {\n\talias /data/web_static/current/;\n}\n"
-sudo sed -i "/server_name _;/a $CONFIG_BLOCK" $CONFIG_FILE
+sudo sed -i '38i\\tlocation /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 
 # Restart Nginx
 sudo service nginx restart
